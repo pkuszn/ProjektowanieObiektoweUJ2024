3cb9e5778c1e768080b741dd04d08dce39202c5d
patryk <patryk.kuszneruk1798@gmail.com>
2024-08-25 14:45:32 +0200

    Improvments of PinsToJson. Corrected logic of IC74HC08 component

--- src/edu/uj/po/simulation/commands/IC74HC08Command.java
+++ src/edu/uj/po/simulation/commands/IC74HC08Command.java
@@ -13,25 +13,25 @@ public class IC74HC08Command implements ComponentCommand {
         HashMap<Integer, ComponentPin> pins = (HashMap<Integer, ComponentPin>) component.getPins();
         if (pins.get(1).getState() == PinState.HIGH || pins.get(2).getState() == PinState.HIGH) {
             pins.get(3).setState(PinState.HIGH);
-        } else {
+        } else if (pins.get(1).getState() != PinState.UNKNOWN && pins.get(2).getState() != PinState.UNKNOWN) {
             pins.get(3).setState(PinState.LOW);
         }
 
         if (pins.get(4).getState() == PinState.HIGH || pins.get(5).getState() == PinState.HIGH) {
             pins.get(6).setState(PinState.HIGH);
-        } else {
+        } else if (pins.get(4).getState() != PinState.UNKNOWN && pins.get(5).getState() != PinState.UNKNOWN) {
             pins.get(6).setState(PinState.LOW);
         }
 
         if (pins.get(9).getState() == PinState.HIGH || pins.get(10).getState() == PinState.HIGH) {
             pins.get(8).setState(PinState.HIGH);
-        } else {
+        } else if (pins.get(9).getState() != PinState.UNKNOWN && pins.get(10).getState() != PinState.UNKNOWN) {
             pins.get(8).setState(PinState.LOW);
         }
 
         if (pins.get(12).getState() == PinState.HIGH || pins.get(13).getState() == PinState.HIGH) {
             pins.get(11).setState(PinState.HIGH);
-        } else {
+        } else if (pins.get(12).getState() != PinState.UNKNOWN && pins.get(13).getState() != PinState.UNKNOWN) {
             pins.get(11).setState(PinState.LOW);
         }
     }
--- src/edu/uj/po/simulation/utils/PinsToJson.java
+++ src/edu/uj/po/simulation/utils/PinsToJson.java
@@ -1,22 +1,32 @@
 package edu.uj.po.simulation.utils;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.UUID;
 
 import edu.uj.po.simulation.models.ComponentPin;
 
 public class PinsToJson {
+    public static final String STATIC_GUID = generateStaticGuid();
+
     public static void saveToJson(int componentId, Map<Integer, ComponentPin> map) {
         String jsonString = mapToJson(map);
         String timestamp = getCurrentTimestamp();
-        String filename = String.format("pins_%d_%s.json", componentId, timestamp);
+
+        File directory = new File("json_files/" + STATIC_GUID);
+        if (!directory.exists()) {
+            directory.mkdirs(); 
+        }
+
+        String filename = String.format("%s/pins_%d_%s.json", directory.getPath(), componentId, timestamp);
         try (FileWriter fileWriter = new FileWriter(filename)) {
             fileWriter.write(jsonString);
-            System.out.println("Map serialized to JSON and saved to file.");
+            System.out.println("Map serialized to JSON and saved to file: " + filename);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,11 +48,15 @@ public class PinsToJson {
         }
 
         if (!map.isEmpty()) {
-            // Remove the last comma
             sb.setLength(sb.length() - 1);
         }
 
         sb.append("}");
         return sb.toString();
+    }
+
+    private static String generateStaticGuid() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
     }
 }
